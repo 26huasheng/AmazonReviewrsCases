851e7654bc48d287b703dafeea4cae9d10f90e97
26huasheng <erdongliqing@163.com>
2026-09-01 08:39:18 -0700

    feat: expose case build interfaces

---
+++ case_build/__init__.py
@@ -0,0 +1,9 @@
+from .pipeline import CaseDiscoveryPipeline, CaseShelfBuilder
+from .time_windows import TimeBox, parse_time_boxes
+
+__all__ = [
+    "CaseDiscoveryPipeline",
+    "CaseShelfBuilder",
+    "TimeBox",
+    "parse_time_boxes",
+]
